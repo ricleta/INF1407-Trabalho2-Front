f9ac25bd51cc8136c0c15162ca5e78567e6ff90a
Ricardo Bastos Leta Vieira <47068518+ricleta@users.noreply.github.com>
2025-11-22 19:06:47 +0000

    Fixing navbar links

--- frontend/Typescript/routes.ts
+++ frontend/Typescript/routes.ts
@@ -4,20 +4,20 @@
  */
 export const navLinks = {
     public: {
-        home: { text: 'Home', href: 'index.html' },
-        all_games: { text: 'Games', href: 'Games/listaGames.html' },
+        home: { text: 'Home', href: '/index.html' },
+        all_games: { text: 'Games', href: '/Games/listaGames.html' },
     },
     developer: {
-        my_games: { text: 'My Games', href: 'Games/home_page_games.html' },
-        create_game: { text: 'Create Game', href: 'Games/criaGames.html' },
+        my_games: { text: 'My Games', href: '/Games/home_page_games.html' },
+        create_game: { text: 'Create Game', href: '/Games/criaGames.html' },
     },
     reviewer: {
-        my_reviews: { text: 'My Reviews', href: 'Reviews/listaReviews.html' },
-        create_review: { text: 'Create Review', href: 'Reviews/criaReview.html' },
+        my_reviews: { text: 'My Reviews', href: '/Reviews/listaReviews.html' },
+        create_review: { text: 'Create Review', href: '/Reviews/criaReview.html' },
     },
     authActions: {
-        login: { text: 'Login', href: 'Seguranca/login.html' },
-        register: { text: 'Register', href: 'Seguranca/signup.html' },
-        logout: { text: 'Logout', href: 'Seguranca/logout.html' },
+        login: { text: 'Login', href: '/Seguranca/login.html' },
+        register: { text: 'Register', href: '/Seguranca/signup.html' },
+        logout: { text: 'Logout', href: '/Seguranca/logout.html' },
     }
 };
